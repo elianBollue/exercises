8f12db6c2ab598da37ab7d3bcc7eb75f1773ca3d
Elian Bollue <elian.bollue@student.ucll.be>
2021-02-16 17:01:21 +0100

    finished basic python

--- 01-basic-python/05-strings/01-format-time/student.py
+++ 01-basic-python/05-strings/01-format-time/student.py
@@ -1,1 +1,3 @@
 # Write your code here
+def format_time(h,m,s):
+    return f'{str(h).rjust(2,"0")}:{str(m).rjust(2,"0")}:{str(s).rjust(2,"0")}'
--- 01-basic-python/08-sets/02-remove-duplicates/student.py
+++ 01-basic-python/08-sets/02-remove-duplicates/student.py
@@ -1,1 +1,16 @@
 # Write your code here
+def remove_duplicates_lists(xs): # for those ultra slow speeds
+    newList = []
+    for x in xs:
+        if x not in newList:
+            newList.append(x)
+    return newList
+
+def remove_duplicates(xs):
+    check = set()
+    newList = []
+    for x in xs:
+        if x not in check:
+            newList.append(x)
+            check.add(x)
+    return newList
